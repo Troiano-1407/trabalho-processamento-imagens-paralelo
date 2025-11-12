d021c979b9af8f3f7a87f54f02ada4696c9d76d0
Felipe Troiano <felipetroiano04@gmail.com>
2025-11-11 23:16:15 -0300

    Adiciona .gitignore e código principal

--- processamento_imagens/metricas.py
+++ processamento_imagens/metricas.py
@@ -1,15 +1,12 @@
-# Substitua pelos seus valores reais
-tempo_seq = 47.99     # tempo do processamento_sequencial.py
-tempo_par = 40.64     # tempo do processamento_paralelo.py
-n_cores = 6          # número de núcleos do seu processador
+tempo_seq = 545.92     
+tempo_par = 42.11     
+n_cores = 6          
 
-# Cálculos
 speedup = tempo_seq / tempo_par
 eficiencia = speedup / n_cores
-throughput_seq = 60000 / tempo_seq   # se usou todas as imagens do CIFAR-10
+throughput_seq = 60000 / tempo_seq   
 throughput_par = 60000 / tempo_par
 
-# Exibir resultados
 print(f"Speedup: {speedup:.2f}x")
 print(f"Eficiência: {eficiencia*100:.2f}%")
 print(f"Throughput Sequencial: {throughput_seq:.2f} imagens/s")
